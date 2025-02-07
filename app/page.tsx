b3c552399f4131b5d7c7a5c60bdf9f9c505fbb74
Pierpaolo Laurito <pierpaolo.laurito@gmail.com>
2025-02-07 18:52:21 +0100

    Update landing page: change logo text, update links, remove About, add Contacts page

--- app/page.tsx
+++ app/page.tsx
@@ -26,11 +26,6 @@ export default function Home() {
                 </Link>
               </li>
               <li>
-                <Link href="/about" className="hover:text-gray-300 transition-colors">
-                  About
-                </Link>
-              </li>
-              <li>
                 <Link href="/contact" className="hover:text-gray-300 transition-colors">
                   Contact
                 </Link>
